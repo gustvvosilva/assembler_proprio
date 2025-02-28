ec4ccaed519efa91df36a7bb17c8e4265761daf0
gustvvosilva <gustavosilvadeoliveira02@gmail.com>
2025-02-28 14:54:26 -0300

    estudando o melhor jeito

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void limpa_linha(char *linha) {
+    for (int i = 0; linha[i] != 0; i++)
+        linha[i] = 0;
+    return;
+}
+
+int main() {
+    
+    FILE *file = fopen("entrada.txt", "r");
+    if(file == NULL) return -1;
+
+    char caractere[1];
+    char linha[8];
+    int i = 0;
+
+    for(;;) {
+
+        if(!fread(caractere, 1, 1, file))
+            break;
+
+        printf("C %c\n", *caractere);
+
+        if(*caractere != 32 && *caractere != 10)
+        {
+            linha[i] = *caractere;
+            i++;
+        }
+        else
+        {
+            printf("L %s\n", linha);
+            printf("kkkkkkk %d\n", strcmp(linha, "ADD"));
+            limpa_linha(linha);
+            i = 0;
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
